737b80fb10ea27828195d2ab4376c8d88b94de9f
Thibault Scorielle <46597542+Genessy@users.noreply.github.com>
2024-03-17 15:51:56 +0100

    feat(connection page) (#2)

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  template: `
+<div class="w-96 p-4 bg-gray-200 border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+  <ng-content ></ng-content>
+</div>
+`,
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+}
