f2374b63de43458dc9bb6896280c815f2990839d
Eliseo Murillo <eliseomurillo9@gmail.com>
2024-03-17 14:23:01 +0100

    init flowbiote and tailwinds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <h1 class="text-3xl text-red-500 font-bold border border-red-300">HOLA AMIGO</h1>
+  `
 })
 export class AppComponent {
   title = 'BiblioTech';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
