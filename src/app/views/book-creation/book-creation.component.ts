43c505295e73678014be4e23c6fb46490e95934d
Thibault Scorielle <thibault.scorielle@gmail.com>
2024-03-17 16:38:43 +0100

    feat(book creation)

--- src/app/views/book-creation/book-creation.component.ts
+++ src/app/views/book-creation/book-creation.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-book-creation',
   standalone: true,
-  imports: [],
-  templateUrl: './book-creation.component.html',
-  styleUrl: './book-creation.component.css'
+  imports: [CardComponent, RouterModule],
+  template: ` <app-card>
+    <form class="space-y-6 p-8" action="#">
+      <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+        Créer un livre
+      </h5>
+      <div>
+        <label
+          for="title"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Titre du livre</label
+        >
+        <input
+          type="title"
+          name="title"
+          id="title"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+          placeholder="Angular"
+          required
+        />
+      </div>
+      <div>
+        <label
+          for="resume"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Description</label
+        >
+        <textarea
+          id="resume"
+          rows="4"
+          class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Write your description here..."
+        ></textarea>
+      </div>
+      <button
+        [routerLink]="['/']"
+        type="submit"
+        class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Créer le livre
+      </button>
+    </form>
+  </app-card>`,
+  styleUrl: './book-creation.component.css',
 })
-export class BookCreationComponent {
-
-}
+export class BookCreationComponent {}
